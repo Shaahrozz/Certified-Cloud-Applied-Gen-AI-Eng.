8b250f49f0ed7ad5234b8cf1e5a00ba71ffda293
Shaahrozz <115104694+Shaahrozz@users.noreply.github.com>
2024-02-17 05:28:34 +0500

    Delete Task1 directory

--- Task1/hello.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-//Write a program to print hello world
-let messsage = "Hello World!";
-console.log(messsage);
--- Task1/hello.ts
+++
@@ -1,4 +0,0 @@
-//Write a program to print hello world
-
-let messsage : string = "Hello World!"
-console.log(messsage);
