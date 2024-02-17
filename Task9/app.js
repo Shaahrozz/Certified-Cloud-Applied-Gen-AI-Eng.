e69a1bb6f52c23cb67813273bd93fc050d0ca022
Shaahrozz <115104694+Shaahrozz@users.noreply.github.com>
2024-02-17 05:33:17 +0500

    Delete Task9 directory

--- Task9/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-//Favorite Number: Store your favorite number in a variable. Then, using that variable, 
-//create a message that reveals your favorite number. Print that message.
-let favorite_no = 3;
-console.log("my favorite no. is:", favorite_no);
--- Task9/app.ts
+++
@@ -1,7 +0,0 @@
-//Favorite Number: Store your favorite number in a variable. Then, using that variable, 
-//create a message that reveals your favorite number. Print that message.
-
-
-let favorite_no: number = 3
-console.log("my favorite no. is:", favorite_no);
-
